fcab0698ef2bec618039a1f28ac042b27269191d
Felipe Brandão <brandaofelipebrandao@gmail.com>
2023-06-02 15:35:00 -0300

    10.5 Implemente a função createMenu, bem como seus casos de teste

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,10 +1,8 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 const createMenu = (object) => ({
-  fetchMenu: () => ({
-    food: object.food,
-    drinks: object.drinks,
-  }),
+  fetchMenu: () => (object),
+  consumption: [],
 });
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -3,14 +3,19 @@ const createMenu = require('../src/restaurant');
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
     // Escreva todos os testes aqui.
-    // Escreva dois testes, um que verifica se a função createMenu() retorna um objeto que possui a chave fetchMenu e outro verificando se o valor da chave fetchMenu do objeto retornado pela função createMenu() é uma função.
+    // 1. Escreva dois testes, um que verifica se a função createMenu() retorna um objeto que possui a chave fetchMenu e outro verificando se o valor da chave fetchMenu do objeto retornado pela função createMenu() é uma função.
     expect(Object.keys(createMenu())[0]).toBe('fetchMenu');
     expect(typeof createMenu().fetchMenu).toBe('function');
 
-    // Escreva um teste que verifica se o objeto retornado pela função createMenu({ food: {}, drinks: {} }).fetchMenu() retorna um objeto cujas chaves são somente food e drinks.
-    expect(Object.keys(createMenu({ food: {}, drinks: {} }).fetchMenu()).length).toBe(2);
+    // 2. Escreva um teste que verifica se o objeto retornado pela função createMenu({ food: {}, drinks: {} }).fetchMenu() retorna um objeto cujas chaves são somente food e drinks.
+    expect(Object.keys(createMenu({ food: {}, drinks: {} }).fetchMenu()).length).toEqual(2);
     expect(Object.keys(createMenu({ food: {}, drinks: {} }).fetchMenu())[0]).toBe('food');
     expect(Object.keys(createMenu({ food: {}, drinks: {} }).fetchMenu())[1]).toBe('drinks');
+
+    //  3. Escreva um teste que verifica se o menu passado pra função createMenu() é idêntico ao menu recuperado pela função createMenu({ food: {}, drinks: {} }).fetchMenu().
+
+    // 5. Escreva um teste que verifica se a propriedade consumption do objeto retornado pela função createMenu({ food: {}, drinks: {} }), após a criação do menu, retorna um array vazio.
+    expect(Array.isArray(createMenu({ food: {}, drinks: {} }).consumption)).toBe(true);
+    expect(createMenu({ food: {}, drinks: {} }).consumption.length).toEqual(0);
   });
 });
-    // 
