0e08ff8747e8ed938edf2badb88383c8ec52aded
Felipe Brandão <brandaofelipebrandao@gmail.com>
2023-06-01 21:27:36 -0300

    10.1 Implemente a função createMenu, bem como seus casos de teste

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,10 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
-
-const createMenu = () => {};
+const createMenu = (object) => ({
+  fetchMenu: () => ({
+    food: object.food,
+    drinks: object.drinks,
+  }),
+});
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -2,7 +2,10 @@ const createMenu = require('../src/restaurant');
  
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // Escreva todos os testes aqui.
+    // Escreva dois testes, um que verifica se a função createMenu() retorna um objeto que possui a chave fetchMenu e outro verificando se o valor da chave fetchMenu do objeto retornado pela função createMenu() é uma função.
+    expect(Object.keys(createMenu())[0]).toBe('fetchMenu');
+    expect(typeof createMenu().fetchMenu).toBe('function');
   });
 });
+    // 
