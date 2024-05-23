96199d5c93ea0c401226363e523ea8d2f1de1d70
felipe <brandaofelipebrandao@gmail.com>
2024-05-23 17:28:27 -0300

    Testes para 1-average

---
+++ tests/1-average.spec.js
@@ -0,0 +1,14 @@
+const average = require('../src/1-average');
+
+describe('Testes da função `average`', () => {
+  it('Verifica se a função `average` tem o comportamento esperado', () => {
+    expect(average([1, 1])).toBe(1);
+    expect(average([2, 4])).toBe(3);
+    expect(average([10000, 20000, 30000])).toBe(20000);
+  });
+
+  it('Verifica se a função `average` retorna `undefined` caso o array de entrada possua elementos não-numéricos', () => {
+    expect(average([1, '2'])).toBeUndefined();
+    expect(average([1, 10, 100, true])).toBeUndefined();
+  })
+})
