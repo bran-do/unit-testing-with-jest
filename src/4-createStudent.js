e13c7abfcdccecfe60d2c27781a32aa2680d7e29
felipe <brandaofelipebrandao@gmail.com>
2024-05-23 17:37:19 -0300

    Testes da função createStudent

--- src/4-createStudent.js
+++ src/4-createStudent.js
@@ -2,7 +2,7 @@
   A função createStudent recebe como parâmetro um nome, e retorna um objeto contendo duas chaves:
 
     (1) name, contendo o nome passado como parâmetro;
-    (2) feedback, contendo uma função que retorna a frase 'Eita pessoa boa!' ao ser chamada.
+    (2) feedback, contendo uma função que retorna a frase 'Essa é uma pessoa muito bacana!' ao ser chamada.
 
   Implemente a função de forma que ela atenda aos testes propostos.
 
@@ -12,7 +12,7 @@
     const estudante = createStudent('Leandrão, o Lobo Solitário')
 
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
-    estudante.feedback() // Retorna: 'Eita pessoa boa!'
+    estudante.feedback() // Retorna: 'Essa é uma pessoa muito bacana!'
 */
 function feedbackFunction() {
   return 'Essa é uma pessoa muito bacana!';
@@ -21,7 +21,7 @@ function feedbackFunction() {
 const createStudent = (nome) => {
   const obj = {
     name: nome,
-    feedback: feedbackFunction,
+    feedback: feedbackFunction(),
   };
   return obj;
 };
--- tests/1-average.spec.js
+++ tests/1-average.spec.js
@@ -1,7 +1,7 @@
 const average = require('../src/1-average');
 
 describe('Testes da função `average`', () => {
-  it('Verifica se a função `average` tem o comportamento esperado', () => {
+  it('Verifica se a função `average` retorna a média entre os números do array de entrada', () => {
     expect(average([1, 1])).toBe(1);
     expect(average([2, 4])).toBe(3);
     expect(average([10000, 20000, 30000])).toBe(20000);
@@ -10,5 +10,5 @@ describe('Testes da função `average`', () => {
   it('Verifica se a função `average` retorna `undefined` caso o array de entrada possua elementos não-numéricos', () => {
     expect(average([1, '2'])).toBeUndefined();
     expect(average([1, 10, 100, true])).toBeUndefined();
-  })
-})
+  });
+});
